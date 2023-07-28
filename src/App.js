02e0c680a8d61e29b3014019b4f3adc22ebf6414
jadewapi <125937514+jadewapi@users.noreply.github.com>
2023-07-27 20:54:53 -0500

    state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,454 @@
-export default function App() {
-  return <div>2323</div>;
+import { useState } from "react";
+function App() {
+  const [data, setData] = useState([
+    {
+      teacherName: "Jade Pineda",
+      userName: "jp",
+      userPin: "1111",
+      assignments: [
+        {
+          assignmentName: "Dichotomous Key",
+          assignmentDescription:
+            "Create a dichotomous key to classify different types of leaves.",
+          assignmentAverage: 89,
+        },
+        {
+          assignmentName: "Solar System Model",
+          assignmentDescription:
+            "Build a scale model of the solar system, including all planets and their relative distances from the Sun.",
+          assignmentAverage: 59,
+        },
+        {
+          assignmentName: "Chemical Reactions",
+          assignmentDescription:
+            "Investigate and document three chemical reactions that produce noticeable changes.",
+          assignmentAverage: 78,
+        },
+        {
+          assignmentName: "Ecosystem Research",
+          assignmentDescription:
+            "Choose an ecosystem and research its components, interactions, and the importance of biodiversity within it.",
+          assignmentAverage: 98,
+        },
+      ],
+      allStudents: [
+        {
+          firstName: "Emma",
+          lastName: "Thompson",
+          average: 82,
+          studentId: "111",
+          studentAssignment: [
+            {
+              assignmentName: "Dichotomous Key",
+              assignmentDescription:
+                "Create a dichotomous key to classify different types of leaves.",
+              score: 76,
+            },
+            {
+              assignmentName: "Solar System Model",
+              assignmentDescription:
+                "Build a scale model of the solar system, including all planets and their relative distances from the Sun.",
+              score: 78,
+            },
+            {
+              assignmentName: "Chemical Reactions",
+              assignmentDescription:
+                "Investigate and document three chemical reactions that produce noticeable changes.",
+              score: 53,
+            },
+            {
+              assignmentName: "Ecosystem Research",
+              assignmentDescription:
+                "Choose an ecosystem and research its components, interactions, and the importance of biodiversity within it.",
+              score: 87,
+            },
+          ],
+        },
+        {
+          firstName: "Noah",
+          lastName: "Johnson",
+          average: 82,
+          studentId: "187",
+          studentAssignment: [
+            {
+              assignmentName: "Dichotomous Key",
+              assignmentDescription:
+                "Create a dichotomous key to classify different types of leaves.",
+              score: 18,
+            },
+            {
+              assignmentName: "Solar System Model",
+              assignmentDescription:
+                "Build a scale model of the solar system, including all planets and their relative distances from the Sun.",
+              score: 75,
+            },
+            {
+              assignmentName: "Chemical Reactions",
+              assignmentDescription:
+                "Investigate and document three chemical reactions that produce noticeable changes.",
+              score: 13,
+            },
+            {
+              assignmentName: "Ecosystem Research",
+              assignmentDescription:
+                "Choose an ecosystem and research its components, interactions, and the importance of biodiversity within it.",
+              score: 67,
+            },
+          ],
+        },
+        {
+          firstName: "Olivia",
+          lastName: "Smith",
+          average: 92,
+          studentId: "002",
+          studentAssignment: [
+            {
+              assignmentName: "Dichotomous Key",
+              assignmentDescription:
+                "Create a dichotomous key to classify different types of leaves.",
+              score: 91,
+            },
+            {
+              assignmentName: "Solar System Model",
+              assignmentDescription:
+                "Build a scale model of the solar system, including all planets and their relative distances from the Sun.",
+              score: 78,
+            },
+            {
+              assignmentName: "Chemical Reactions",
+              assignmentDescription:
+                "Investigate and document three chemical reactions that produce noticeable changes.",
+              score: 10,
+            },
+            {
+              assignmentName: "Ecosystem Research",
+              assignmentDescription:
+                "Choose an ecosystem and research its components, interactions, and the importance of biodiversity within it.",
+              score: 53,
+            },
+          ],
+        },
+        {
+          firstName: "Liam",
+          lastName: "Williams",
+          average: 58,
+          studentId: "191",
+          studentAssignment: [
+            {
+              assignmentName: "Dichotomous Key",
+              assignmentDescription:
+                "Create a dichotomous key to classify different types of leaves.",
+              score: 75,
+            },
+            {
+              assignmentName: "Solar System Model",
+              assignmentDescription:
+                "Build a scale model of the solar system, including all planets and their relative distances from the Sun.",
+              score: 98,
+            },
+            {
+              assignmentName: "Chemical Reactions",
+              assignmentDescription:
+                "Investigate and document three chemical reactions that produce noticeable changes.",
+              score: 53,
+            },
+            {
+              assignmentName: "Ecosystem Research",
+              assignmentDescription:
+                "Choose an ecosystem and research its components, interactions, and the importance of biodiversity within it.",
+              score: 27,
+            },
+          ],
+        },
+        {
+          firstName: "Ava",
+          lastName: "Johnson",
+          average: 78,
+          studentId: "771",
+          studentAssignment: [
+            {
+              assignmentName: "Dichotomous Key",
+              assignmentDescription:
+                "Create a dichotomous key to classify different types of leaves.",
+              score: 66,
+            },
+            {
+              assignmentName: "Solar System Model",
+              assignmentDescription:
+                "Build a scale model of the solar system, including all planets and their relative distances from the Sun.",
+              score: 58,
+            },
+            {
+              assignmentName: "Chemical Reactions",
+              assignmentDescription:
+                "Investigate and document three chemical reactions that produce noticeable changes.",
+              score: 13,
+            },
+            {
+              assignmentName: "Ecosystem Research",
+              assignmentDescription:
+                "Choose an ecosystem and research its components, interactions, and the importance of biodiversity within it.",
+              score: 97,
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+  return (
+    <>
+      {/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/}
+      <Navbar />
+      {/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/}
+      <Buttons />
+      {/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/}
+      <AllStudents />
+      {/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/}
+      <DisplayInterface />
+      {/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/}
+      <AssignmentInfo />
+      {/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/}
+    </>
+  );
+}
+export default App;
+
+function Navbar() {
+  return (
+    <nav class="topMenu">
+      <div class="logoContainer">
+        <img src="aida.png" alt="logo" />
+        <p>AIDA</p>
+      </div>
+      <form class="login" onSubmit={(e) => handleSubmitTeacherLogin(e)}>
+        <input
+          type="text"
+          class="userName"
+          placeholder="user name"
+          onChange={(e) => setUserName(e.target.value)}
+          value={userName}
+        />
+        <input
+          type="text"
+          class="userPin"
+          placeholder="pin"
+          onChange={(e) => setUserPin(e.target.value)}
+          value={userPin}
+        />
+        <button class="loginButton">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="3rem"
+            viewBox="0 0 448 512"
+          >
+            <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+          </svg>
+        </button>
+      </form>
+    </nav>
+  );
+}
+
+function Buttons() {
+  return (
+    <>
+      <div class="students">
+        <p>Students</p>
+      </div>
+      <div class="interface">
+        <button class="interfaceButon">Interface</button>
+      </div>
+      <div class="manageAssignment">
+        <button class="assignmentsButton">Assignments</button>
+      </div>
+      <div class="menu">
+        <p>Menu</p>
+      </div>
+    </>
+  );
+}
+
+function AllStudents() {
+  return (
+    <section class="allStudents">
+      <div class="specificStudent">
+        <div class="specificStudentContainer">
+          <p class="specificStudentFirstName">Jadsfdsfde</p>
+          <p class="specificStudentLastName">Pineda</p>
+          <p class="specificStudentScore">98</p>
+        </div>
+      </div>
+      <div class="specificStudent">
+        <div class="specificStudentContainer">
+          <p class="specificStudentFirstName">Jadsfdsfde</p>
+          <p class="specificStudentLastName">Pineda</p>
+          <p class="specificStudentScore">98</p>
+        </div>
+      </div>
+      <div class="specificStudent">
+        <div class="specificStudentContainer">
+          <p class="specificStudentFirstName">Jadsfdsfde</p>
+          <p class="specificStudentLastName">Pineda</p>
+          <p class="specificStudentScore">98</p>
+        </div>
+      </div>
+      <div class="specificStudent">
+        <div class="specificStudentContainer">
+          <p class="specificStudentFirstName">Jadsfdsfde</p>
+          <p class="specificStudentLastName">Pineda</p>
+          <p class="specificStudentScore">98</p>
+        </div>
+      </div>
+      <div class="specificStudent">
+        <div class="specificStudentContainer">
+          <p class="specificStudentFirstName">Jadsfdsfde</p>
+          <p class="specificStudentLastName">Pineda</p>
+          <p class="specificStudentScore">98</p>
+        </div>
+      </div>
+      <div class="specificStudent">
+        <div class="specificStudentContainer">
+          <p class="specificStudentFirstName">Jadsfdsfde</p>
+          <p class="specificStudentLastName">Pineda</p>
+          <p class="specificStudentScore">98</p>
+        </div>
+      </div>
+      <div class="specificStudent">
+        <div class="specificStudentContainer">
+          <p class="specificStudentFirstName">Jadsfdsfde</p>
+          <p class="specificStudentLastName">Pineda</p>
+          <p class="specificStudentScore">98</p>
+        </div>
+      </div>
+      <div class="specificStudent">
+        <div class="specificStudentContainer">
+          <p class="specificStudentFirstName">Jadsfdsfde</p>
+          <p class="specificStudentLastName">Pineda</p>
+          <p class="specificStudentScore">98</p>
+        </div>
+      </div>
+      <div class="specificStudent">
+        <div class="specificStudentContainer">
+          <p class="specificStudentFirstName">Jadsfdsfde</p>
+          <p class="specificStudentLastName">Pineda</p>
+          <p class="specificStudentScore">98</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function DisplayInterface() {
+  return (
+    <section class="displayInterface">
+      <div class="interfaceDisplay">
+        <div class="studentID">
+          <p>student id:</p>
+          <p>239</p>
+        </div>
+        <div class="name">
+          <p>Jade</p>
+          <p>Pineda</p>
+        </div>
+        <div class="studentAverage">
+          <p>student avg:</p>
+          <p>87</p>
+        </div>
+        <div class="entry">
+          <p>Entry</p>
+        </div>
+        <div class="assignment">
+          <p>Assignment</p>
+        </div>
+        <div class="grade">
+          <p>Grade</p>
+        </div>
+        <div class="assignmentContainer">
+          <div class="specificAssignment">
+            <div class="entryNumber">
+              <p>100</p>
+            </div>
+            <div class="specificAssignmentInfo">
+              <div>
+                <p>Assignment Name</p>
+              </div>
+              <p>Assignment Description</p>
+            </div>
+            <div class="assignmentGrade">
+              <p>100</p>
+              <p>A+</p>
+            </div>
+          </div>
+          <div class="specificAssignment">
+            <div class="entryNumber">
+              <p>100</p>
+            </div>
+            <div class="specificAssignmentInfo">
+              <div>
+                <p>Assignment Name</p>
+              </div>
+              <p>Assignment Description</p>
+            </div>
+            <div class="assignmentGrade">
+              <p>100</p>
+              <p>A+</p>
+            </div>
+          </div>
+          <div class="specificAssignment">
+            <div class="entryNumber">
+              <p>100</p>
+            </div>
+            <div class="specificAssignmentInfo">
+              <div>
+                <p>Assignment Name</p>
+              </div>
+              <p>Assignment Description</p>
+            </div>
+            <div class="assignmentGrade">
+              <p>100</p>
+              <p>A+</p>
+            </div>
+          </div>
+          <div class="specificAssignment">
+            <div class="entryNumber">
+              <p>100</p>
+            </div>
+            <div class="specificAssignmentInfo">
+              <div>
+                <p>Assignment Name</p>
+              </div>
+              <p>Assignment Description</p>
+            </div>
+            <div class="assignmentGrade">
+              <p>100</p>
+              <p>A+</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function AssignmentInfo() {
+  return (
+    <section class="assignmentInfo">
+      <div class="specificAssignmentMenu">
+        <p>Assignment Name</p>
+        <div class="specificStats">
+          <p>avg:</p>
+          <p>87</p>
+        </div>
+      </div>
+      <div class="listOfStudents">
+        <div class="studentNameAssignment">
+          <div class="studentName">
+            <p>Jade</p>
+            <p>Pineda</p>
+          </div>
+          <input type="text" />
+        </div>
+      </div>
+    </section>
+  );
 }
